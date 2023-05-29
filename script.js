7a263f0070b1e05664cb25c7f081e47e417617c8
pppp49943 <134843364+pppp49943@users.noreply.github.com>
2023-05-29 10:25:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+
+fetch('rate.xml')
+  .then(response => response.text())
+  .then(data => {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(data, 'text/xml');
+
+    
+    const currencies = xmlDoc.getElementsByTagName('Currency');
+    const rates = {};
+    for (let i = 0; i < currencies.length; i++) {
+      const currency = currencies[i];
+      const code = currency.getAttribute('Code');
+      const rate = parseFloat(currency.getAttribute('Rate'));
+      rates[code] = rate;
+    }
+
+  
+    const form = document.getElementById('converterForm');
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      const amountInput = document.getElementById('amount');
+      const amount = parseFloat(amountInput.value);
+      if (isNaN(amount)) {
+        alert('請輸入有效數值！');
+        resetForm();
+        return;
+      }
+      const resultContainer = document.getElementById('resultContainer');
+      resultContainer.innerHTML = '';
+
+      const currencies = ['USD', 'EUR', 'JPY', 'KRW', 'AUD'];
+      currencies.forEach(currency => {
+        const convertedAmount = (amount / rates['TWD'] * rates[currency]).toFixed(2);
+        const resultText = `${convertedAmount} ${currency}`;
+        const resultElement = document.createElement('p');
+        resultElement.textContent = resultText;
+        resultContainer.appendChild(resultElement);
+      });
+      resetForm();
+    });
+
+    const resetBtn = document.getElementById('resetBtn');
+    resetBtn.addEventListener('click', resetForm);
+
+
+    function resetForm() {
+      form.reset();
+      const resultContainer = document.getElementById('resultContainer');
+      resultContainer.innerHTML = '';
+    }
+  });
